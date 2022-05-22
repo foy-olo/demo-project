de9ad857dec755a67a9bfe2b0c047357fee30550
foy-olo <foysolo@gmail.com>
2022-05-22 18:17:27 +0100

    Fix port

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def hello():
         Revision=revision)
 
 if __name__ == '__main__':
-    server_port = os.environ.get('PORT', '8080')
+    server_port = os.environ.get('PORT', 8080)
     app.run(debug=False, port=server_port, host='0.0.0.0')
