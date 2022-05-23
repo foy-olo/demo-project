57cb80085ac6862bc195ec2731a3fe59308a95b5
foy-olo <foysolo@gmail.com>
2022-05-23 09:28:58 +0100

    Add vpc connector

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,3 +1,8 @@
+resource "google_project_service" "vpcaccess_api" {
+  service  = "vpcaccess.googleapis.com"
+  disable_on_destroy = false
+}
+
 #Main VPC
 resource "google_compute_network" "vpc_network" {
   name                    = "main-vpc-network"
@@ -12,3 +17,12 @@ resource "google_compute_subnetwork" "private" {
   network       = google_compute_network.vpc_network.id
   private_ip_google_access = true
   }
+
+# VPC access connector
+resource "google_vpc_access_connector" "connector" {
+  name          = "vpcconn"
+  region        = var.location
+  ip_cidr_range = "10.1.0.0/8"
+  network       = google_compute_network.vpc_network.name
+  depends_on    = [google_project_service.vpcaccess_api]
+}
